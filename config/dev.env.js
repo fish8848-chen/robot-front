abfc6ef3561caffc272f8cdcd052bd27c49d6463
chentuo <625903911@qq.com>
2019-09-21 22:22:51 +0800

    修改接口地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8843"',
+  BASE_API: '"http://101.132.138.61:8843"',
 })
